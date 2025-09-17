0e12c37ec5eecad9277f9e86637df9dc5ffc051b
Ashry Ramadhan <ashry.ramadhan@webscientia.id>
2025-09-17 04:11:12 +0000

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ python3 -m pip install textual
 # Create src directory if it doesn't exist
 mkdir -p src
 
+# Source ROS2 environment
+source /opt/ros/humble/setup.bash
+
 # Clone ros2sysmon if it doesn't exist
 if [ ! -d "src/ros2sysmon" ]; then
     echo "Cloning ros2sysmon..."
@@ -14,9 +17,6 @@ else
     echo "ros2sysmon already exists, skipping clone..."
 fi
 
-# Source ROS2 environment
-source /opt/ros/humble/setup.bash
-
 # Build the package
 echo "Building ros2sysmon..."
 colcon build --packages-select ros2sysmon
@@ -24,5 +24,3 @@ colcon build --packages-select ros2sysmon
 # Source the workspace
 echo "Sourcing workspace..."
 source install/setup.bash
-
-echo "Setup complete! You can now run: ros2 run ros2sysmon ros2sysmon"
